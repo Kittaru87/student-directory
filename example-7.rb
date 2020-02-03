f9f7461832894df9c79cdc5206d0ddee3dd33f99
Kittaru87 <Kittaru87@gmail.com>
2020-02-03 15:45:31 +0000

    name & cohort together, default values for empty input, adding typo method and single student detail method

---
+++ example-7.rb
@@ -0,0 +1,134 @@
+# In the input_students method the cohort value is hard-coded.
+# How can you ask for both the name and the cohort?
+# What if one of the values is empty?
+# Can you supply a default value? The input will be given to you as a string?
+# How will you convert it to a symbol? What if the user makes a typo?
+
+def input_students
+  puts "Please enter the name and cohort of the students"
+  puts "To finish, just hit return twice"
+  # empty array
+  students = []
+  # while loop for getting information
+  while true do
+    name = gets.chomp
+    if name == ""
+      break
+    end
+    #pulling the cohort from the input and turning into a symbol
+    cohort = name.split.last.to_sym
+    #removing cohort from the name
+    name = name.split(' ')[0...-1].join(' ')
+    puts "Enter country of birth"
+    country = gets.chomp
+      country = "n/a" if country == ""
+    puts "Enter height"
+    height = gets.chomp
+      height = "n/a" if height == ""
+    puts "Enter hobbies"
+    hobbies = gets.chomp
+      hobbies = "n/a" if hobbies == ""
+    # add the student hash to the array
+    students << {name: name, cohort: cohort, country: country, height: height, hobbies: hobbies}
+    puts "Now we have #{students.count} students"
+    puts "Please enter the next student name and cohort"
+    end
+   # returns the array of students
+  students
+end
+
+# print out students beginning with a certain letter
+def which_letter(students)
+  puts "Show students beginning with [input letter]"
+  letter = gets.chomp
+  students.each do |student|
+    if student[:name].start_with?(letter.upcase)
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def print_header
+  puts "The students of Villains Academy".center(50)
+  puts "-----------------".center(50)
+end
+
+# prints all the students details with a centered layout
+def print(students)
+  students.each_with_index do |student, index|
+    puts "#{index+1}".center(50)
+    puts "#{student[:name]}".center(50)
+    puts "(#{student[:cohort]} cohort)".center(50)
+    puts "County of birth: #{student[:country]}".center(50)
+    puts "Height: #{student[:height]}".center(50)
+    puts "Hobbies: #{student[:hobbies]}".center(50)
+    puts "\n"
+  end
+end
+
+#prints a single student's details with a centered layour
+def print_single(student)
+  puts "#{student[:name]}".center(50)
+  puts "(#{student[:cohort]} cohort)".center(50)
+  puts "County of birth: #{student[:country]}".center(50)
+  puts "Height: #{student[:height]}".center(50)
+  puts "Hobbies: #{student[:hobbies]}".center(50)
+end
+
+def print_footer(students)
+  puts "\n"
+  puts "Overall, we have #{students.count} great students".center(50)
+end
+
+# prints out student names of length less than 12
+def short_name(students)
+  students.each do |student|
+    if student[:name].length < 12
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+# typo method
+def typo(students)
+  puts "\nIs all the information accurate? Y/N"
+  input = gets.chomp
+
+  while true do
+    if input == "Y"
+      roll_call(students)
+      break
+    elsif input == "N"
+      puts "What student's information do you want to correct?"
+      student_info = gets.chomp
+      students.each_with_index do |student, index|
+        if student_info == student[:name]
+          print_single(student)
+          puts "\nWhat category do you want to change - name, cohort, country, height or hobbies?"
+          category_info = gets.chomp
+          student.each do |key, value|
+            if category_info.to_sym == key
+              puts "Enter correction"
+              correction = gets.chomp
+              student[key] = correction
+              print_single(student)
+            end
+          end
+        end
+      end
+    puts "\nIs all the information accurate? Y/N"
+    input = gets.chomp
+    end
+  end
+end
+
+# printing the heading/student list and footer in one method
+def roll_call(students)
+  print_header
+  print(students)
+  print_footer(students)
+end
+
+students = input_students
+roll_call(students)
+typo(students)
