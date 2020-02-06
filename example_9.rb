55cd4437609d0d6dac8813b67cb17d6f24beeeab
Kittaru87 <Kittaru87@gmail.com>
2020-02-06 13:15:58 +0000

    simplifying the code to remove cohort method, additional inputs

--- example_9.rb
+++ example_9.rb
@@ -3,36 +3,27 @@
 # when appropriate and plural form otherwise?
 
 def input_students
-  puts "Please enter the name and cohort of the student, separated by a comma"
+  puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # empty array
+  # create an empty array
   students = []
-  # while loop for getting information
-  while true do
-    name = gets.chomp
-    break if name == ""
-    #pulling the cohort from the input and turning into a symbol
-    cohort = name.split.last
-    #removing cohort and comma from the name
-    name = name.split(' ')[0...-1].join(' ').chop
-    puts "Enter country of birth"
-    country = gets.chomp
-      country = "n/a" if country == ""
-    puts "Enter height"
-    height = gets.chomp
-      height = "n/a" if height == ""
-    puts "Enter hobbies"
-    hobbies = gets.chomp
-      hobbies = "n/a" if hobbies == ""
+  # get the first names
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    puts "Please enter the cohort"
+    cohort = gets.chomp
+    puts "November" if cohort == ""
     # add the student hash to the array
-    students << {name: name, cohort: cohort, country: country, height: height, hobbies: hobbies}
-    puts "Now we have #{students.count} student" if students.count == 1
-    puts "Now we have #{students.count} students" if students.count == 0 || students.count > 1
+    students << {name: name, cohort: cohort}
+    students.count == 1 ? (puts "Now we have #{students.count} student") : (puts "Now we have #{students.count} students")
     puts "Please enter the next student name and cohort"
-    end
+    # get another name from the user
+    name = gets.chomp
+   end
    # returns the array of students
-  students
-end
+   students
+ end
 
 def print_header
   puts "The students of Villains Academy".center(50)
@@ -51,10 +42,6 @@ end
 def print_single(student)
   puts "#{student[:name]}".center(50)
   puts "(#{student[:cohort]} cohort)".center(50)
-  puts "County of birth: #{student[:country]}".center(50)
-  puts "Height: #{student[:height]}".center(50)
-  puts "Hobbies: #{student[:hobbies]}".center(50)
-  puts "\n"
 end
 
 #prints the footer with student count
@@ -63,51 +50,29 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students\n" if students.count == 0 || students.count > 1
 end
 
-# pulling a list of current cohorts
-def cohort_list(students)
-    cohort_list = []
-    students.each {|student| cohort_list << student[:cohort]}
-    current_cohorts = cohort_list.uniq
-    puts "Current cohorts:"
-    current_cohorts.each {|cohort| puts cohort}
-end
-
-# showing the students in a selected cohort
-def students_in_cohort(students)
-  puts "\nWhich cohort would you like to view?"
-  input = gets.chomp
-  cohort_count = 0
-  cohort_array = Array.new { [] }
-  students.each do |student|
-    student.each { |key, value| (cohort_count += 1; cohort_array << student ) if value == input }
-  end
-  roll_call(cohort_array)
-  cohort_array
-end
 
 # typo method
-def typo(cohorts)
+def typo(students)
   puts "\nIs all the information accurate? Y/N"
   input = gets.chomp
   while true do
     # if the info is correct, print out list again
-    (roll_call(cohorts); break) if input == "Y"
+    (roll_call(students); break) if input == "Y"
     if input == "N"
       puts "Which student's information do you want to correct?"
       student_info = gets.chomp
-      cohorts.each_with_index do |student, index|
+      students.each_with_index do |student, index|
         if student_info == student[:name]
-        print_single(student)
-        puts "What category do you want to change - name, cohort, country, height or hobbies?"
-        category_info = gets.chomp
-        student.each do |key, value|
-          if category_info.to_sym == key
-            puts "Enter correction"
-            correction = gets.chomp
-            student[key] = correction
-            print_single(student)
+          print_single(student)
+          puts "\nWhat category do you want to change?"
+          category_info = gets.chomp
+          student.each do |key, value|
+            if category_info.to_sym == key
+              puts "Enter correction"
+              correction = gets.chomp
+              student[key] = correction
+            end
           end
-        end
         end
       end
     puts "\nIs all the information accurate? Y/N"
@@ -125,6 +90,5 @@ def roll_call(students)
 end
 
 students = input_students
-cohort_list(students)
-cohorts = students_in_cohort(students)
-typo(cohorts)
+roll_call(students)
+typo(students)
